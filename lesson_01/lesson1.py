3b96e838bbeb18ca01b5d79c50332d5584e1a44f
Fedor <fedor@y-nevidomskiy.local>
2024-06-02 15:34:45 +0300

    add firs homework

---
+++ lesson_01/lesson1.py
@@ -0,0 +1,34 @@
+# age = 10
+#
+# print(type(10))
+# print(type(age))
+#
+# name = 'Alex' + ' ' + 'Mayor'
+# print(type('Alex'))
+# print(type(name))
+#
+# print(name)
+# print(name.upper())
+
+# age = 20
+# print(id(20))
+# print(id(age))
+# print(age + 10)
+# print(20 + 10)
+
+alex_age = 30
+mu_age = alex_age
+your_page = 30
+
+print(alex_age)
+print(mu_age)
+print(your_page)
+print('*************************/')
+alex_age = 20
+
+mu_age = alex_age
+
+print(alex_age)
+print(mu_age)
+print(your_page)
+print('*************************/')
